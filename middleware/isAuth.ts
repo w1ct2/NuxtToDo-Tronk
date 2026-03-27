2c1d0585f298ec2e1173481d35ad249e17941424
W1ct2 <renskove@gmail.com>
2026-03-27 21:38:03 +0300

    Доработка авторизации. (Рефакторинг middleware isAuth)

--- middleware/isAuth.ts
+++ middleware/isAuth.ts
@@ -1,7 +1,15 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    const isAuth = true
-  
-    if (!isAuth) {
-      return navigateTo('/login')
-    }
-  })
+export default defineNuxtRouteMiddleware((to) => {
+  if (!import.meta.client) {
+    return;
+  }
+
+  const token = localStorage.getItem("token");
+
+  if (!token && to.path !== "/login") {
+    return navigateTo("/login");
+  }
+
+  if (token && to.path === "/login") {
+    return navigateTo("/");
+  }
+});
