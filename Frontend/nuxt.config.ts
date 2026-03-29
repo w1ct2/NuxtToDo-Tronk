3c21c99a7e6a5c8bcd01ebbc25d623c77240099c
W1ct2 <renskove@gmail.com>
2026-03-29 16:19:15 +0300

    Отключение NuxtDevTools

--- Frontend/nuxt.config.ts
+++ Frontend/nuxt.config.ts
@@ -1,6 +1,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   runtimeConfig: {
     public: {
       apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:5001/api',
